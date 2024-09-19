68be8c20099bcaa2d7609753469bcf571585d8e3
SyedFazal110 <syedfazal808@gmail.com>
2024-09-19 12:15:47 +0500

    Work Updated

--- react todo app firebase/src/Layout.jsx
+++ react todo app firebase/src/Layout.jsx
@@ -1,8 +1,36 @@
-import React from 'react'
+import React, { useRef, useState } from 'react'
 
 const Layout = () => {
+  const [todo, settodo] = useState([])
+  let inputRef = useRef()
+
+  function addTodo(event) {
+    event.preventDefault()
+    if (inputRef.current.value === '') {
+      alert("Please Enter a Value!")
+    } else {
+      todo.unshift(inputRef.current.value)
+      settodo([...todo])
+      inputRef.current.value = ''
+    }
+  }
+
+
   return (
-    <div>Layout</div>
+    <>
+      <div>
+        <h4 className='text-center'>React Todo App</h4>
+        <form onSubmit={addTodo}>
+          <input id='inputTodo' type="text" placeholder='Enter Todo' ref={inputRef} />
+          <button type='submit'>Add</button>
+        </form>
+        <div>
+          <ul>{todo.map((item) => (
+            <li>{item}</li>
+          ))}</ul>
+        </div>
+      </div>
+    </>
   )
 }
 
